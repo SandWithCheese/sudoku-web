a1c3703d54720839c0cbd915961343ac173f30cf
SandWithCheese <naufalahmad022@gmail.com>
2023-05-22 16:10:19 +0700

    web-sudoku 1.0

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 async function getBoard() {
-    const data = await fetch("https://sudoku-api.vercel.app/api/dosuku")
+    const data = await fetch("https://sugoku.onrender.com/board?difficulty=random")
     jsonData = await data.json()
+    console.log(jsonData.board)
     deleteInputs()
-    insertArr(jsonData.newboard.grids[0].value)
-    console.log(jsonData.newboard.grids[0].value)
+    insertArr(jsonData.board)
 }
 
-// getBoard()
-
-// console.log(getBoard())
 
 function insertArr(arr){
     let baris = 0
@@ -18,7 +15,6 @@ function insertArr(arr){
 
         if (arr[baris][kolom] !== 0){
             inputs[i].value = arr[baris][kolom]
-            console.log(arr[baris][kolom])
         }
 
         if (kolom === 8){
@@ -27,6 +23,7 @@ function insertArr(arr){
     }
 }
 
+
 function deleteInputs(){
     const inputs = document.querySelectorAll("input")
     inputs.forEach(input => {
@@ -34,8 +31,125 @@ function deleteInputs(){
     })
 }
 
-// fetch("https://sudoku-api.vercel.app/api/dosuku")
-// .then(res => res.json())
-// .then(data => insertArr(data.newboard.grids[0].value))
 
-// const inputs = Array.prototype.slice.call(document.querySelectorAll('.box-1 input'))
+function getBoardArr(){
+    let board = []
+    let kolom = []
+    const inputs = document.querySelectorAll("input")
+    inputs.forEach((input, index) => {
+        if (index % 9 === 0 && index !== 0) {
+            board.push(kolom)
+            kolom = []
+        }
+
+        if (input.value === ""){
+            kolom.push(0)
+        }
+        else{
+            kolom.push(Number(input.value))
+        }
+    })
+    board.push(kolom)
+    
+    return board
+}
+
+
+function valid(board, num, pos){
+    for (let i = 0; i < 9; i++){
+        if (board[pos[0]][i] === num && pos[1] !== i){
+            return false
+        }
+    }
+
+    for (let j = 0; j < 9; j++){
+        if (board[j][pos[1]] === num && pos[0] !== j){
+            return false
+        }
+    }
+
+    const box_x = Math.floor(pos[1]/3)
+    const box_y = Math.floor(pos[0]/3)
+
+    for (let i = box_y * 3; i < box_y * 3 + 3; i++){
+        for(let j = box_x * 3; j < box_x * 3 + 3; j++){
+            if (board[i][j] === num && pos[0] !== i && pos[1] !== j){
+                return false
+            }
+        }
+    }
+
+    return true
+}
+
+
+function find_empty(board){
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board.length; j++){
+            if (board[i][j] === 0){
+                return [i, j]
+            }
+        }
+    }
+
+    return false
+}
+
+
+function solveBoard(){
+    let board = getBoardArr()
+    function solve(board){
+        let find = find_empty(board)
+        let baris, kolom;
+    
+        if (find === false){
+            return true
+        }
+        else{
+            [baris, kolom] = find
+        }
+    
+        for (let i = 1; i <= 9; i ++){
+            if (valid(board, i, [baris, kolom])){
+                const inputs = document.querySelectorAll("input")
+                board[baris][kolom] = i
+                inputs[baris*9 + kolom].value = i
+    
+                if (solve(board)){
+                    return true
+                }
+                
+                board[baris][kolom] = 0
+                inputs[baris*9 + kolom].value = ""
+            }
+        }
+    
+        return false
+    
+    }
+
+    solve(board)
+}
+
+
+function check(){
+    let board = getBoardArr()
+
+    if (!find_empty(board)){
+        for (let i = 0; i < 9; i ++){
+            for (let j = 0; j < 9; j ++){
+                if (!valid(board, board[i][j], [i, j])){
+                    console.log(i, j)
+                    alert("It's not a valid solution")
+                    return NaN
+                }
+            }
+        }
+
+        alert("Hurray! You found the solution")
+    }
+    else{
+        alert("The game hasn't finished")
+    }
+}
+
